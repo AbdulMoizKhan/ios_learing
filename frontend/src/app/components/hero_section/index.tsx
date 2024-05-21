d0a02a681f88c55ee70e0846a8424f61de6b500c
Abdul Moiz Khan <73781652+AbdulMoizKhan@users.noreply.github.com>
2024-05-22 01:17:44 +0500

    z

--- frontend/src/app/components/hero_section/index.tsx
+++ frontend/src/app/components/hero_section/index.tsx
@@ -4,35 +4,32 @@ import React from 'react'
 import hero from '../../../../public/hero.jpg';
 
 const heroStack: SxProps = {
-    // display: 'flex',
-    // flexDirection: { xs: 'column-reverse', md:'row' },
-    // justifyContent: 'center',
-    // alignItems: 'center',
     height: '100%',
-    display:'flex',
+    display: 'flex',
     flexDirection: 'column',
-    alignItems:'center'
-    // maxWidth:'400px'
+    alignItems: 'center'
 };
 const box1: SxProps = {
     width: { xs: '100%' },
     position: 'absolute',
     zIndex: '1',
-}
+};
 const box2: SxProps = {
     width: { xs: '100%' },
     pt: { xs: '20px', md: '0px' },
     position: 'relative'
-}
+};
 
 const HeroSection = () => {
     return (
         <div>
-            <Container sx={{ height: '50vh' , position:'absolute' , zIndex:'1', maxWidth:'100% !important'  }}>
+            <Container sx={{ height: '50vh', position: 'absolute', zIndex: '1', maxWidth: '100% !important' }}>
                 <Box sx={heroStack}>
-                    <Stack direction={'column'} spacing={3} sx={{ height: '100%', maxWidth:'500px' }} justifyContent={'center'} alignItems={{ xs: "center" }}>
-                        <Typography variant='h5' sx={{ fontWeight: 'bold', color: '#fff' , textAlign:'center' }}>At Izafatech, we're dedicated to empowering tech enthusiasts of all ages to become digital superheroes.  
-                        <span style={{ color: '#d6fc3c' }}> "Wow, I built that!"</span>,  we're here to ensure you don’t just learn tech, but love it. Let’s build amazing things and maybe break the internet (just a little)!</Typography>
+                    <Stack direction={'column'} spacing={3} sx={{ height: '100%', maxWidth: '500px' }} justifyContent={'center'} alignItems={{ xs: "center" }}>
+                        <Typography variant='h5' sx={{ fontWeight: 'bold', color: '#fff', textAlign: 'center' }}>
+                            At Izafatech, we&apos;re dedicated to empowering tech enthusiasts of all ages to become digital superheroes.
+                            <span style={{ color: '#d6fc3c' }}> &quot;Wow, I built that!&quot;</span>, we&apos;re here to ensure you don&apos;t just learn tech, but love it. Let&apos;s build amazing things and maybe break the internet (just a little)!
+                        </Typography>
                         <Button variant="contained" sx={{
                             background: '#d6fc3c',
                             color: '#000',
@@ -47,9 +44,9 @@ const HeroSection = () => {
                     </Stack>
                 </Box>
             </Container>
-            <Image src={hero} alt="hero" width={576} style={{ maxWidth: '-webkit-fill-available', height: '50vh', width: '100%', position: 'relative', filter:'brightness(0.5)' }} />
+            <Image src={hero} alt="hero" width={576} style={{ maxWidth: '-webkit-fill-available', height: '50vh', width: '100%', position: 'relative', filter: 'brightness(0.5)' }} />
         </div>
-    )
+    );
 }
 
-export default HeroSection
+export default HeroSection;
